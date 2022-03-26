09ce782fe3ab82f1becbdc48a71467d3a1473f91
Rasool Poursangari <raspsgri@gmail.com>
2022-03-26 23:09:37 +0100

    a

---
+++ src/Start/Main.java
@@ -0,0 +1,18 @@
+package Start;
+public class Main {
+    static float numOne = -10.07f;
+    static int   randNum = (int) (Math.random() * 100);
+    static Methods methods = new Methods();
+    public static void main(String[] args) {
+        methods.looperOne(randNum);
+        methods.printer(numOne);
+        methods.adder(3);
+        methods.printer(numOne);
+        methods.printer(randNum);
+        methods.testerOne(randNum);
+        methods.testerTwo(randNum);
+        methods.testerThree(randNum);
+        methods.sumRekShow(randNum);
+        methods.randomizer();
+    }
+}
---
+++ src/Start/Methods.java
@@ -0,0 +1,49 @@
+package Start;
+public class Methods {
+    public void printer(float x) {
+        System.out.println(Math.abs(x));
+    }
+    public void adder(int x) {
+        Main.numOne += x;
+    }
+    public void randomizer() {
+        Main.randNum = (int) (Math.random() * 100);
+    }
+    public void testerOne(float x) {
+        if (x > 50.0) {
+            System.out.println("Bo");
+        }else{
+            System.out.println("Ku");
+        }
+    }
+    public void testerTwo(float x) {
+        int r  = (x>50) ? 1 : 0;
+        System.out.println(r);
+    }
+    public void testerThree(float x) {
+        boolean r  = x > 50;
+        System.out.println(r);
+    }
+    public void looperOne(int x) {
+
+        System.out.println("Random startNumber -> " + x);
+        int m = Main.randNum;
+        while (m > 0) {
+            System.out.println(m);
+            m --;
+        }
+    }
+    public int  sumRek(int k) {
+        if (k > 0) {
+            return k + sumRek(k - 1);
+        } else {
+            return 0;
+        }
+    }
+    public void sumRekShow(int x) {
+        int result;
+        result = sumRek(x);
+        System.out.println("sumRek: " + result);
+    }
+}
+
